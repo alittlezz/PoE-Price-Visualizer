a2fc955b2b018b39316b92e4efbc4e34fd89ab40
Valentin <valentinwhite2@gmail.com>
2018-09-05 15:15:12 +0300

    new requirements.txt

--- main.py
+++ main.py
@@ -9,92 +9,97 @@ league_name = "Delve"
 
 app = dash.Dash()
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
 
-df = pd.read_csv("Unique Data.csv")
-dfHC = pd.read_csv("Unique Data HC.csv")
-headers = list(df.columns.values)
+@app.route('/')
+def index():
+	return "<h1>HEROKU</h1>"	
 
-unique_names = open("uniqueNames.txt", "r").readlines()
-unique_names = list(map(lambda name : name.rstrip(), unique_names))
+# colors = {
+#     'background': '#111111',
+#     'text': '#7FDBFF'
+# }
 
-app.layout = html.Div(
-	style={'backgroundColor': colors['background'], "height" : "100vh"}, 
-	children=[
-	    html.H1(
-	        children='Unique price viewer',
-	        style={
-	            'textAlign': 'center',
-	            'color': colors['text']
-	        }
-	    ),
+# df = pd.read_csv("Unique Data.csv")
+# dfHC = pd.read_csv("Unique Data HC.csv")
+# headers = list(df.columns.values)
 
-	    html.Div(children='A web application for viewing the price changes of uniques', 
-	    		 style={
-			        'textAlign': 'center',
-			        'color': colors['text']
-	    		}
-	    ),
-	    html.Div(
-		    children = dcc.Dropdown(id = "input",
-		    			 options = [
-		    			 	{"label" : name, "value" : name} for name in unique_names
-		    			 ],
-		    			 placeholder = "Select unique"
-		   			   ),
-		    style = {
-		    	"width" : "25vw"
-		    }
-	    ),
+# unique_names = open("uniqueNames.txt", "r").readlines()
+# unique_names = list(map(lambda name : name.rstrip(), unique_names))
 
-	    html.Div(id = "output-graph")
-	]
-)
+# app.layout = html.Div(
+# 	style={'backgroundColor': colors['background'], "height" : "100vh"}, 
+# 	children=[
+# 	    html.H1(
+# 	        children='Unique price viewer',
+# 	        style={
+# 	            'textAlign': 'center',
+# 	            'color': colors['text']
+# 	        }
+# 	    ),
+
+# 	    html.Div(children='A web application for viewing the price changes of uniques', 
+# 	    		 style={
+# 			        'textAlign': 'center',
+# 			        'color': colors['text']
+# 	    		}
+# 	    ),
+# 	    html.Div(
+# 		    children = dcc.Dropdown(id = "input",
+# 		    			 options = [
+# 		    			 	{"label" : name, "value" : name} for name in unique_names
+# 		    			 ],
+# 		    			 placeholder = "Select unique"
+# 		   			   ),
+# 		    style = {
+# 		    	"width" : "25vw"
+# 		    }
+# 	    ),
+
+# 	    html.Div(id = "output-graph")
+# 	]
+# )
 
 
-@app.callback(
-    Output(component_id='output-graph', component_property='children'),
-    [Input(component_id='input', component_property='value')]
-)
+# @app.callback(
+#     Output(component_id='output-graph', component_property='children'),
+#     [Input(component_id='input', component_property='value')]
+# )
 
-def update(input_data):
-	if input_data in headers:
-		return dcc.Graph(
-	    	id = "graph_1",
-			figure = {
-			"data" : [
-				{'x' : df["Date"], 
-				 'y' : df[input_data], 
-				 "type" : "line", 
-				 "name" : league_name},
-				{'x' : dfHC["Date"],
-				 'y' : dfHC[input_data],
-				 "type" : "line",
-				 "name" : league_name + " Hardcore"}
-			],
-			'layout': {
-				"title" : input_data,
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                },
-                "yaxis" : {
-                	"title" : "Chaos orbs"
-                }
-            }
-		})
-	# else:
-	# 	return html.Div(
-	# 		id = "graph_1",
-	# 		style = {
-	# 			"color" : colors["background"],
-	# 			"height" : "500px"
-	# 		}
-	# 	)
+# def update(input_data):
+# 	if input_data in headers:
+# 		return dcc.Graph(
+# 	    	id = "graph_1",
+# 			figure = {
+# 			"data" : [
+# 				{'x' : df["Date"], 
+# 				 'y' : df[input_data], 
+# 				 "type" : "line", 
+# 				 "name" : league_name},
+# 				{'x' : dfHC["Date"],
+# 				 'y' : dfHC[input_data],
+# 				 "type" : "line",
+# 				 "name" : league_name + " Hardcore"}
+# 			],
+# 			'layout': {
+# 				"title" : input_data,
+#                 'plot_bgcolor': colors['background'],
+#                 'paper_bgcolor': colors['background'],
+#                 'font': {
+#                     'color': colors['text']
+#                 },
+#                 "yaxis" : {
+#                 	"title" : "Chaos orbs"
+#                 }
+#             }
+# 		})
+# 	# else:
+# 	# 	return html.Div(
+# 	# 		id = "graph_1",
+# 	# 		style = {
+# 	# 			"color" : colors["background"],
+# 	# 			"height" : "500px"
+# 	# 		}
+# 	# 	)
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
