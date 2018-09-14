658f7f777ab14bf691423c85a2b16b5e615c9da3
Valentin <valentinwhite2@gmail.com>
2018-09-14 23:26:27 +0300

    added database

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def add_unique(name, league, price, date):
 	db.session.commit()
 
 db.create_all()
-db.add_unique("Vali Heart", "Delve", 999.95, datetime.date.today())
+add_unique("Vali Heart", "Delve", 999.95, datetime.date.today())
 
 app = dash.Dash(server = server)
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
