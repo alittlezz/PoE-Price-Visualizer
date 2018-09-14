665681325dc56754e337d677672acfae23c4f64c
Valentin <valentinwhite2@gmail.com>
2018-09-14 22:28:48 +0300

    added database

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 import pandas as pd
-import datetime
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 league_name = "Delve"
 
 server = Flask(__name__)
+server.config["SQLALCHEMY_DATABASE_URI"] = "postgres://ybosjhdyfhfhct:f2695864f05a7f9ceb35416bde81348b581cdf40f3cdad3d8cc915b8e97e029c@ec2-54-83-50-145.compute-1.amazonaws.com:5432/d59l2q0k6pnf1u"
+db = SQLAlchemy(server)
+
+class Unique(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	league = db.Column(db.String(30))
+	name = db.Column(db.String(35))
+	price = db.Column(db.Float)
+	date = db.Column(db.Date)	
+
+	def __init__(self, name, league, price, date):
+		self.name = name
+		self.league = league
+		self.price = price
+		self.date = date
+
+def add_unique(name, league, price, date):
+	unique = Unique(name, league, price, date)
+	db.session.add(unique)
+	db.session.commit()
+
+db.create_all()
+db.add_unique("Vali Heart", "Delve", 999.95, datetime.date.today())
+
 app = dash.Dash(server = server)
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
@@ -35,7 +60,7 @@ app.layout = html.Div(
 	        }
 	    ),
 
-	    html.Div(children='A web application for viewing the price changes of uniques', 
+	    html.Div(children=Unique.query.first().name, 
 	    		 style={
 			        'textAlign': 'center',
 			        'color': colors['text']
--- test2.py
+++ test2.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 import datetime
 
 server = Flask(__name__)
-server.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:SlJAFTBO@localhost/Path_of_Exile_Visualizer_DB"
+server.config["SQLALCHEMY_DATABASE_URI"] = "postgres://ybosjhdyfhfhct:f2695864f05a7f9ceb35416bde81348b581cdf40f3cdad3d8cc915b8e97e029c@ec2-54-83-50-145.compute-1.amazonaws.com:5432/d59l2q0k6pnf1u"
 db = SQLAlchemy(server)
 
 class Unique(db.Model):
@@ -27,6 +27,11 @@ def add_unique(name, league, price, date):
 	unique = Unique(name, league, price, date)
 	db.session.add(unique)
 	db.session.commit()
+
+def get_uniques():
+	results = Unique.query.filter(Unique.price <= 300, Unique.league == "Delve")
+	for unique in results:
+		print(unique.name)
 
 app = dash.Dash(__name__, server = server)
 app.layout = html.Div(
